37bdd8b2ad5592fe1f8d090d86c327412e054221
esbiyasemin <yaseminkisa94@gmail.com>
2024-03-19 22:53:13 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    public class SepetManager
+    {
+        //naming convention - isimlendirme Kuralı
+        //Metot adı büyük harfle başlar
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete Eklendi : " +urun.Adi);
+        }
+        public void Ekle2(string urunAdi,string aciklama,double fiyat,int stokAdedi)
+        {
+            Console.WriteLine("Sepete Eklendi : " + urunAdi);
+        }
+    }
+}
